96ef4ce714c89781ad512cbfd052d19ff6ff37cc
Gerdus Marais <gerdusx@gmail.com>
2023-03-31 15:47:54 +0200

    Rewards, Escrow

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -9,8 +9,15 @@ contract Escrow {
     Booking bookingContract;
     IERC20 usdcToken;
 
-    mapping(uint256 => uint256) public bookingAmounts; //BookingId -> amount in escrow
-    mapping(uint256 => bool) public isBookingPaid; //BookingID -> funds released to host
+    // A mapping that stores the deposited amount (in USDC tokens) for each booking ID
+    // Key: Booking ID
+    // Value: Amount of USDC tokens held in escrow for the booking
+    mapping(uint256 => uint256) public bookingAmounts;
+    
+    // A mapping that tracks whether the funds for a booking have been released to the property owner
+    // Key: Booking ID
+    // Value: A boolean flag indicating if the funds have been released (true) or not (false)
+    mapping(uint256 => bool) public isBookingPaid;
 
     constructor(address _propertyContractAddress, address _bookingContractAddress, address _usdcTokenAddress) {
         propertyContract = Property(_propertyContractAddress);
@@ -18,26 +25,52 @@ contract Escrow {
         usdcToken = IERC20(_usdcTokenAddress);
     }
 
+    /**
+     * @dev Stores the deposited amount for a booking in the escrow contract.
+     * This function is called by the Booking contract after it has transferred
+     * the required USDC tokens to the escrow contract.
+     *
+     * @param bookingId The ID of the booking associated with the deposited amount.
+     * @param amount The amount of USDC tokens deposited for the booking.
+     */
     function deposit(uint256 bookingId, uint256 amount) external {
+        // Ensure that the booking amount has not been deposited before
         require(bookingAmounts[bookingId] == 0, "Booking amount already deposited");
-        bookingAmounts[bookingId] = amount;
 
-        // Transfer USDC tokens to the escrow contract
-        usdcToken.transferFrom(msg.sender, address(this), amount);
+        // Store the deposited amount for the given booking ID
+        bookingAmounts[bookingId] = amount;
     }
 
+    /**
+     * @dev Releases the deposited amount for a booking to the property owner.
+     * This function transfers the USDC tokens held in the escrow contract for
+     * the booking to the property owner's address.
+     *
+     * @param bookingId The ID of the booking for which the amount should be released.
+     */
     function release(uint256 bookingId) external {
+        // Ensure that the booking amount has not been released before
         require(!isBookingPaid[bookingId], "Booking amount already released");
+
+        // Get the deposited amount for the given booking ID
         uint256 amount = bookingAmounts[bookingId];
+
+        // Ensure there is a booking amount to release
         require(amount > 0, "No booking amount to release");
 
+        // Get the property ID associated with the booking ID
         uint256 propertyId = bookingContract.getPropertyIdByBookingId(bookingId);
+
+        // Get the property owner's address
         address propertyOwner = propertyContract.getPropertyOwner(propertyId);
+
+        // Ensure the property owner's address is valid
         require(propertyOwner != address(0), "Property owner not found");
 
         // Transfer USDC tokens from the escrow contract to the property owner
         usdcToken.transfer(propertyOwner, amount);
 
+        // Mark the booking amount as released
         isBookingPaid[bookingId] = true;
     }
 }
---
+++ contracts/Rewards.sol
@@ -0,0 +1,110 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Rewards is Ownable {
+    // This mapping stores the points earned by a user in a specific month.
+    mapping(address => mapping(uint256 => uint256)) public userMonthPoints;
+
+    // This mapping stores the total points earned by all users in a specific month.
+    mapping(uint256 => uint256) public totalMonthPoints;
+
+    // This mapping stores the total rewards (in USDC) allocated for a specific month.
+    mapping(uint256 => uint256) public totalMonthRewards;
+
+    // This mapping keeps track of whether a user has withdrawn their rewards for a specific month.
+    mapping(address => mapping(uint256 => bool)) public userRewardsWithdrawn;
+
+    address public usdcTokenAddress;
+
+    event UserPointsAdded(address indexed user, uint256 points, uint256 month);
+    event RewardWithdrawn(address indexed user, uint256 amount, uint256 month);
+
+    // This modifier checks if the given month is completed.
+    // It ensures that users can only withdraw rewards from a completed month.
+    modifier onlyCompletedMonth(uint256 month) {
+        require(month < block.timestamp / 30 days, "Can only withdraw rewards from a completed month");
+        _;
+    }
+
+    // This modifier checks if the user has not withdrawn their rewards for the given month.
+    // It ensures that users cannot withdraw their rewards more than once for the same month.
+    modifier notWithdrawn(uint256 month) {
+        require(!userRewardsWithdrawn[msg.sender][month], "Rewards already withdrawn for this month");
+        _;
+    }
+
+    constructor(address _usdcTokenAddress)  {
+        usdcTokenAddress = _usdcTokenAddress;
+    }
+
+    // This function adds points to a user for the current month.
+    function addUserPoints(address user, uint256 points) external {
+        // Calculate the current month based on the block timestamp.
+        uint256 currentMonth = block.timestamp / 30 days;
+
+        // Add the given points to the user's points for the current month.
+        userMonthPoints[user][currentMonth] += points;
+
+        // Add the given points to the total points for the current month.
+        totalMonthPoints[currentMonth] += points;
+
+        // Emit an event
+        emit UserPointsAdded(user, points, currentMonth);
+    }
+
+    // This function allows users to withdraw their rewards for a specified month based on a rolling 3-month rewards calculation.
+    function withdrawUserRewards(uint256 month) external onlyCompletedMonth(month) notWithdrawn(month) {
+        // Calculate the start month for the rolling 3-month rewards calculation.
+        uint256 startMonth = (month >= 2) ? (month - 2) : 0;
+
+        // Initialize variables for tracking variables (Used to calculate rewards)
+        uint256 totalPoints = 0;
+        uint256 totalRewards = 0;
+        uint256 userPoints = 0;
+
+        // Iterate through the 3-month period (from startMonth to the specified month, inclusive).
+        // Adding the totals to tracking variables
+        for (uint256 i = startMonth; i <= month; i++) {
+            totalPoints += totalMonthPoints[i];
+            totalRewards += totalMonthRewards[i];
+            userPoints += userMonthPoints[msg.sender][i];
+        }
+
+        // Ensure the user has points within the rolling 3-month period.
+        require(userPoints > 0, "No rewards to withdraw");
+
+        // Calculate the user's reward based on their points and the total points in the 3-month period.
+        uint256 userReward = (totalRewards * userPoints) / totalPoints;
+
+        // Mark the user's rewards as withdrawn for the specified month.
+        userRewardsWithdrawn[msg.sender][month] = true;
+
+        // Transfer the calculated reward amount from the contract's address to the user's address.
+        IERC20 usdcToken = IERC20(usdcTokenAddress); 
+        usdcToken.transferFrom(address(this), msg.sender, userReward);
+
+        // Emit an event
+        emit RewardWithdrawn(msg.sender, userReward, month);
+    }
+
+    // This function adds rewards to the contract for the current month.
+    function addRewards() public payable {
+        // Calculate the current month based on the block timestamp.
+        uint256 currentMonth = block.timestamp / 30 days;
+
+        // Add the received ether (in wei) to the total rewards for the current month.
+        totalMonthRewards[currentMonth] += msg.value;
+    }
+
+    // It calls the addRewards function to ensure the received ether is added to the rewards pool.
+    receive() external payable {
+        addRewards();
+    }
+
+    // It calls the addRewards function to ensure the received ether is added to the rewards pool.
+    fallback() external payable {
+        addRewards();
+    }
+}
